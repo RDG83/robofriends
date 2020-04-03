353196ebce47e31a6912fad67dfb552bad383ba3
RDG83 <rdgrijs@hotmail.com>
2020-04-03 15:35:00 +0200

    set up project

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div className="tc">
+                <h1 className="bg-gold">Hello World</h1>
+                <p className="bg-light-purple">Welcome to React!</p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
