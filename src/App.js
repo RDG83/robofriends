d6d526c4aa8d70e0d8ee747be4bf9c61a9cc1a97
RDG83 <rdgrijs@hotmail.com>
2020-04-07 11:19:55 +0200

    add scroll to robots

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
     constructor() {
@@ -15,7 +16,7 @@ class App extends Component {
     componentDidMount() {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
-            .then(users => this.setState({robots: users}));
+            .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = event => {
@@ -32,7 +33,9 @@ class App extends Component {
             <div className="tc">
                 <h1 className="f1">robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
             </div>
         );
     }
