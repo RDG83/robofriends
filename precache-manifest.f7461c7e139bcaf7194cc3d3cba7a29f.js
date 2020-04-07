13bfafe5a935a602ba893076b1533d50879b4841
RDG83 <rdgrijs@hotmail.com>
2020-04-07 15:06:23 +0200

    Updates

---
+++ precache-manifest.f7461c7e139bcaf7194cc3d3cba7a29f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014c5402416b92efad2f5f221ff2ada2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "bd8938d753c8b8f9ffc2",
+    "url": "/robofriends/static/css/main.a09be94f.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/robofriends/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8938d753c8b8f9ffc2",
+    "url": "/robofriends/static/js/main.8759134b.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
